aeff35b415ff3f0f759909bb1d894c745e3516af
sateigen <shannon.teigen@gmail.com>
2016-07-24 21:22:07 -0400

    Test and method working to find movie title when given a movie ID number.

--- movie.py
+++ movie.py
@@ -5,3 +5,8 @@ class Movie:
         self.video_date = row[2]
         self.link = row[4]
         self.genre = row[5:]
+
+    def get_movie_name(movie_id, movie_data_list):
+        for movie in movie_data_list:
+            if movie.movie_id == movie_id:
+                return movie.title
